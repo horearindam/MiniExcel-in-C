0ea888ede2e659f928b91a9c2ae6776f7cb36068
barkbuff <connect.arindamhore@gmail.com>
2025-12-30 22:55:20 +0530

    Just a bit of error handling

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void usage(FILE *stream) {
+    fprintf(stream, "Usage: ./main <input.csv>\n");
+}
+
+int main(int argc, char** argv)
 {
-    printf("Hello, MiniExcel!\n");
+    if (argc < 2) {
+        usage(stderr);
+        fprintf(stderr, "Error: Missing input file argument.\n");
+        return EXIT_FAILURE;
+    }
+
+    const char *input_file_path = argv[1];
+
     return 0;
 }
