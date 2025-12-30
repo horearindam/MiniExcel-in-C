f701ce9c63b363cf8e245feff4628a79471732f1
barkbuff <connect.arindamhore@gmail.com>
2025-12-30 20:50:43 +0530

    Push in automated build execution via nobuild

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, MiniExcel!\n");
+    return 0;
+}
---
+++ src/nobuild.c
@@ -0,0 +1,22 @@
+#define NOBUILD_IMPLEMENTATION
+#include "nobuild.h"
+
+#define CFLAGS "-Wall", "-Wextra", "-std=c11", "-pedantic", "-ggdb", "-O2"
+
+int main(int argc, char** argv)
+{
+    GO_REBUILD_URSELF(argc, argv);
+    CMD("cc", CFLAGS, "-o", "../build/main", "main.c");
+
+    if(argc > 1)
+    {
+        if(strcmp(argv[1], "run") == 0)
+        {
+            CMD("../build/main");
+        } else {
+            PANIC("Unknown command: %s\n", argv[1]);
+        }
+    }
+
+    return 0;
+}
